616f5d01fdf27bcd7425e01beef49d637805f21b
Monika Cyprys <monko.projects@gmail.com>
2021-07-01 20:31:23 +0200

    add BEM

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,16 @@
 (function () {
   let isOpen = false;
-  const nav = document.querySelector(".show-nav");
-  const menuNavBtn = document.querySelector(".menu-btn");
+  const nav = document.querySelector(".nav__menu");
+  const menuNavBtn = document.querySelector(".nav__btn");
   menuNavBtn.addEventListener("click", function () {
     isOpen = !isOpen;
     if (isOpen) {
       this.setAttribute("src", "./images/icon-close.svg");
-      nav.classList.add("show-nav-is-open");
+      nav.classList.add("nav__menu--active");
     } else {
       this.style.color = "var (--veryDarkGray)";
       this.setAttribute("src", "./images/icon-hamburger.svg");
-      nav.classList.remove("show-nav-is-open");
+      nav.classList.remove("nav__menu--active");
     }
   });
 })();
