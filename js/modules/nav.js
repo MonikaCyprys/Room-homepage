2d4fcad97207308ffc30dd2806a533e0617e222a
Monika Cyprys <monko.projects@gmail.com>
2021-07-04 10:59:46 +0200

    put slider article to header

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -1,16 +1,16 @@
 export default function navigation() {
-  let isOpen = false;
-  const nav = document.querySelector(".nav__menu");
-  const menuNavBtn = document.querySelector(".nav__btn");
-  menuNavBtn.addEventListener("click", function () {
-    isOpen = !isOpen;
-    if (isOpen) {
-      this.setAttribute("src", "./images/icon-close.svg");
-      nav.classList.add("nav__menu--active");
-    } else {
-      this.style.color = "var (--veryDarkGray)";
-      this.setAttribute("src", "./images/icon-hamburger.svg");
-      nav.classList.remove("nav__menu--active");
-    }
-  });
+  // let isOpen = false;
+  // const nav = document.querySelector(".nav__menu");
+  // const menuNavBtn = document.querySelector(".nav__btn");
+  // menuNavBtn.addEventListener("click", function () {
+  //   isOpen = !isOpen;
+  //   if (isOpen) {
+  //     this.setAttribute("src", "./images/icon-close.svg");
+  //     nav.classList.add("nav__menu--active");
+  //   } else {
+  //     this.style.color = "var (--veryDarkGray)";
+  //     this.setAttribute("src", "./images/icon-hamburger.svg");
+  //     nav.classList.remove("nav__menu--active");
+  //   }
+  // });
 }
